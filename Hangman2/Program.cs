1fdaba5257dd29e0a5cfa1930c0a4e20734480d6
Nico De Witte <nico.dewitte@outlook.com>
2019-11-13 11:54:55 +0100

    Step 2 - generate secret

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -4,6 +4,9 @@ namespace Hangman2
 {
     class Program
     {
+        // STEP 2 - THE SECRET WORD
+        static string secret = "";
+
         // STEP 1 - Show user welcome message
         static void Welcome()
         {
@@ -12,9 +15,16 @@ namespace Hangman2
             Console.WriteLine("The gallows are waiting for you");
         }
 
+        // STEP 2 - Generate secret word
+        static void GenerateSecret()
+        {
+            secret = "Hello World".ToLower();
+        }
+
         static void Main(string[] args)
         {
             Welcome();
+            GenerateSecret();
         }
     }
 }
