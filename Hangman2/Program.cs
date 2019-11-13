cdb16a690cc662fdd920eac183b84c7f6870ac88
Nico De Witte <nico.dewitte@outlook.com>
2019-11-13 12:09:45 +0100

    Step 3 - build the initial revealed secret

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -7,6 +7,9 @@ namespace Hangman2
         // STEP 2 - THE SECRET WORD
         static string secret = "";
 
+        // STEP 3 - Revealed Secret (= current user progress)
+        static string revealedSecret = "";
+
         // STEP 1 - Show user welcome message
         static void Welcome()
         {
@@ -21,10 +24,30 @@ namespace Hangman2
             secret = "Hello World".ToLower();
         }
 
+        // STEP 3 - Build the initial revealed secret
+        // hello world => _ _ _ _ _   _ _ _ _ _
+        static void BuildInitialRevealedSecret()
+        {
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] == ' ')
+                {
+                    revealedSecret += "  ";
+                }
+                else
+                {
+                    revealedSecret += "_ ";
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Welcome();
             GenerateSecret();
+            BuildInitialRevealedSecret();
+
+            Console.WriteLine($"Current user progress: {revealedSecret}");
         }
     }
 }
