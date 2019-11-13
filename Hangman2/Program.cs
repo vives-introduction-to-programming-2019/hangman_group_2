f98a6662ee137a3ffe1f8a4e58b60baa22bfcd68
Nico De Witte <nico.dewitte@outlook.com>
2019-11-13 11:48:55 +0100

    Step 1 - user welcome

--- Hangman2/Program.cs
+++ Hangman2/Program.cs
@@ -4,9 +4,17 @@ namespace Hangman2
 {
     class Program
     {
+        // STEP 1 - Show user welcome message
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to hangman.");
+            Console.WriteLine("Generating secret word to guess");
+            Console.WriteLine("The gallows are waiting for you");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Welcome();
         }
     }
 }
